1b4030c8ba59e9e507d77e85f71e58306af3f5d6
qinjia <qinjia@outlook.com>
2015-09-09 18:48:09 +0800

    delete

--- react-pagination/test/index.jsx
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-
-var Pagination = require('../src/pagination.jsx');
-
-React.render(
-	<Pagination pageSize="10" totalCount="100" current="1"/>,
-	document.getElementById('pagination') 
-);
